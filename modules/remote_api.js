91f8fdbccb584ab6f38b2bf3d5b1bb344812fa25
Louis Orleans <louis@orleans.io>
2015-07-25 01:40:02 -0700

    ready for remote (i think)

--- modules/remote_api.js
+++ modules/remote_api.js
@@ -1,76 +1,41 @@
-var express = require('express');
-var app = express();
-
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
 var remote;
+var socket;
 
 module.exports = {
     init: function (remoteControl) {
         remote = remoteControl;
-        remote.setCallbacks({
-            stateChanged: function (buttonStates) {
-                io.emit('get', {states: buttonStates});
-            }
+
+        // var http = require('http');
+        // var options = {
+        //     host: 'pilights.herokuapp.com:7533',
+        //     path: '/'
+        // };
+        // callback = function(response) {
+        //     var str = '';
+        //     response.on('data', function (chunk) {
+        //         str += chunk;
+        //     });
+        //     response.on('end', function () {
+        //         // TODO find out if connection has been established
+        //
+        //         // eval(str);
+        //
+        //         // setup IO callbacks once we have the script
+        //         // socket = io();
+        //         // socket.on('toggle', function (data) {
+        //         //     remote.toggle(data.button);
+        //         // });
+        //     });
+        // };
+        // http.request(options, callback).end();
+
+
+        var socket = require('socket.io-client')('http://pilights.herokuapp.com:7533');
+        socket.on('connect', function () {
+            console.log('Connected to remote server');
         });
-
-        var server = http.listen(7533, function () {
-            var host = server.address().address;
-            var port = server.address().port;
-
-            console.log('Remote API listening at http://%s:%s', host, port);
+        socket.on('toggle', function (data) {
+            remote.toggle(data.button);
         });
-
-        return this;
     }
 };
-
-io.on('connection', function(socket){
-    console.log('user connected');
-    socket.on('toggle', function (data) {
-        remote.toggle(data.button);
-    });
-    socket.on('set', function (data) {
-        remote.set(data.button, data.state);
-    });
-    socket.on('get', function (data) {
-        if (data !== undefined) {
-            socket.emit('get', {button: data.button, state: remote.get(data.button)});
-        } else {
-            socket.emit('get', {states: remote.get()});
-        }
-    });
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-});
-
-
-
-
-
-
-var http = require('http');
-
-//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-var options = {
-  host: 'www.random.org',
-  path: '/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-};
-
-callback = function(response) {
-  var str = '';
-
-  //another chunk of data has been recieved, so append it to `str`
-  response.on('data', function (chunk) {
-    str += chunk;
-  });
-
-  //the whole response has been recieved, so we just print it out here
-  response.on('end', function () {
-    console.log(str);
-  });
-}
-
-http.request(options, callback).end();
